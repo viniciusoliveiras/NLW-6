8ad85a42f9f41d08d43b1af949238abfaf5da303
viniciusoliveiras <vinitag190@gmail.com>
2021-06-23 16:22:09 -0300

    Aula 02 – Maximum Speed

---
+++ src/pages/Home.tsx
@@ -0,0 +1,61 @@
+import { useHistory } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { useAuth } from '../hooks/useAuth';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push('/rooms/new');
+  }
+
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt='Ilustração simbolizando perguntas e respostas'
+        />
+
+        <strong>Toda pergunta tem uma resposta.</strong>
+
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt='Letmeask' />
+
+          <button className='create-room' onClick={handleCreateRoom}>
+            <img src={googleIconImg} alt='Google Logo' />
+            Crie sua sala com o Google
+          </button>
+
+          <div className='separator'>ou entre em uma sala</div>
+
+          <form>
+            <input type='text' placeholder='Digite o código da sala' />
+
+            <Button type='submit'>Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
+function signInWithGoogle(signInWithGoogle: any) {
+  throw new Error('Function not implemented.');
+}
