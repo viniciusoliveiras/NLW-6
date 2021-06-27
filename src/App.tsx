6e12f7b14dc71ddf2b94c3530faa97941ccc5598
viniciusoliveiras <vinitag190@gmail.com>
2021-06-27 13:34:15 -0300

    themeContext & darkmode home page

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,45 @@ import { Room } from './pages/Room';
 import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { ThemeContextProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthContextProvider>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/rooms/new' component={NewRoom} />
-          <Route path='/rooms/:id' component={Room} />
-          <Route path='/admin/rooms/:id' component={AdminRoom} />
-        </Switch>
-        <Toaster
-          position='top-center'
-          reverseOrder={false}
-          gutter={8}
-          toastOptions={{
-            duration: 3000,
-            style: {
-              background: '#f8f8f8',
-              color: '#29292e',
-            },
-            success: {
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/rooms/new' component={NewRoom} />
+            <Route path='/rooms/:id' component={Room} />
+            <Route path='/admin/rooms/:id' component={AdminRoom} />
+          </Switch>
+          <Toaster
+            position='top-center'
+            reverseOrder={false}
+            gutter={8}
+            toastOptions={{
+              duration: 3000,
               style: {
-                background: '#835afd',
+                background: '#D67EE2',
                 color: '#fff',
               },
-            },
-            error: {
-              style: {
-                background: '#e73f5d',
-                color: '#fff',
+              success: {
+                style: {
+                  background: '#835afd',
+                  color: '#fff',
+                },
               },
-            },
-          }}
-        />
-      </AuthContextProvider>
+              error: {
+                style: {
+                  background: '#e73f5d',
+                  color: '#fff',
+                },
+              },
+            }}
+          />
+        </AuthContextProvider>
+      </ThemeContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
+import logoLightImg from '../assets/images/logo.svg';
+import logoDarkImg from '../assets/images/logo-dark.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
 import '../styles/auth.scss';
 import { FormEvent } from 'react';
 import { database } from '../services/firebase';
+import { useTheme } from '../hooks/useTheme';
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const { theme, toggleTheme } = useTheme();
   const [roomCode, setRoomCode] = useState('');
+
+  useEffect(() => {
+    toast('Clique na logo caso queria alterar o tema', {
+      duration: 3000,
+    });
+  }, []);
 
   async function handleCreateRoom() {
     if (!user) {
@@ -47,7 +56,7 @@ export function Home() {
   }
 
   return (
-    <div id='page-auth'>
+    <div id='page-auth' className={theme}>
       <aside>
         <img
           src={illustrationImg}
@@ -61,7 +70,11 @@ export function Home() {
 
       <main>
         <div className='main-content'>
-          <img src={logoImg} alt='Letmeask' />
+          <img
+            src={theme === 'light' ? logoLightImg : logoDarkImg}
+            alt='Letmeask'
+            onClick={toggleTheme}
+          />
 
           <button className='create-room' onClick={handleCreateRoom}>
             <img src={googleIconImg} alt='Google Logo' />
