7bba03c2b0bbd43b712aab044222a656b1311dce
viniciusoliveiras <vinitag190@gmail.com>
2021-06-27 16:10:58 -0300

    darkmode roomCode

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,6 +1,7 @@
 import toast from 'react-hot-toast';
 
 import copyImg from '../assets/images/copy.svg';
+import { useTheme } from '../hooks/useTheme';
 import '../styles/room-code.scss';
 
 type RoomCodeProps = {
@@ -8,13 +9,15 @@ type RoomCodeProps = {
 };
 
 export function RoomCode({ code }: RoomCodeProps) {
+  const { theme } = useTheme();
+
   function copyRoomCodeToClipboard() {
     navigator.clipboard.writeText(code);
     toast.success('Código da sala copiado');
   }
 
   return (
-    <button className='room-code' onClick={copyRoomCodeToClipboard}>
+    <button className={`room-code ${theme}`} onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt='Copy room code' />
       </div>
