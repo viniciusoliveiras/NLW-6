d4929b9e777599f9f1927b0d9293c5331622dc36
viniciusoliveiras <vinitag190@gmail.com>
2021-06-29 01:20:34 -0300

    service worker working

---
+++ public/script.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
+
+if (window.location.protocol === 'http:') {
+  const requireHTTPS = document.getElementById('requireHTTPS');
+  const link = requireHTTPS.querySelector('a');
+  link.href = window.location.href.replace('http://', 'https://');
+  requireHTTPS.classList.remove('hidden');
+}
